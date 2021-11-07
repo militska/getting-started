b8c2e132231098c426b29c5ac53314d9174cbfc3
militska <cheshirenok@gmail.com>
2021-11-08 00:37:23 +0500

    Добавила проверок

--- temperatures/src/main.rs
+++ temperatures/src/main.rs
@@ -2,25 +2,32 @@ use std::io;
 
 fn main() {
     loop {
+        println!("______");
         println!("Change mode: ");
         println!("1. fn->c");
         println!("2. c->fn");
 
         let mut mode = String::new();
-
         io::stdin()
             .read_line(&mut mode)
             .expect("fail");
 
-        let mode: u32 = match mode.trim().parse() {
+        let mode: i8 = match mode.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
+        match mode {
+            1..=2 =>  print!(""),
+            _ => {
+                println!("Выбран некорректный режим");
+                continue
+            }
+        }
+
+
         println!("Введите температуру");
-
         let mut temp = String::new();
-
         io::stdin()
             .read_line(&mut temp)
             .expect("fail");
@@ -30,10 +37,10 @@ fn main() {
             Err(_) => continue,
         };
 
-        if mode == 1 {
-            println!("{:.5}", (temp - 32) * 5 / 9);
-        } else {
-            println!("{:.2}", temp * 9 / 5 + 32);
+        match mode {
+            1 => { println!("{:.5}", (temp - 32) * 5 / 9) },
+            2 => { println!("{:.2}", temp * 9 / 5 + 32) },
+            _ => continue,
         }
     }
 }
