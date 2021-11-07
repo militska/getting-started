5d726d47901f700db08b26ebda76bec318058543
militska <cheshirenok@gmail.com>
2021-11-08 00:52:46 +0500

    Добавила проверок

--- temperatures/src/main.rs
+++ temperatures/src/main.rs
@@ -7,40 +7,57 @@ fn main() {
         println!("1. fn->c");
         println!("2. c->fn");
 
-        let mut mode = String::new();
-        io::stdin()
-            .read_line(&mut mode)
-            .expect("fail");
-
-        let mode: i8 = match mode.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        match mode {
-            1..=2 =>  print!(""),
-            _ => {
-                println!("Выбран некорректный режим");
-                continue
-            }
+        let (mode, res_mode) = enter_mode();
+        if !res_mode {
+            continue
         }
 
-
-        println!("Введите температуру");
-        let mut temp = String::new();
-        io::stdin()
-            .read_line(&mut temp)
-            .expect("fail");
-
-        let temp: i32 = match temp.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
+        let (temp, res_temp) = enter_temp();
+        if !res_temp {
+            continue
+        }
         match mode {
             1 => { println!("{:.5}", (temp - 32) * 5 / 9) },
             2 => { println!("{:.2}", temp * 9 / 5 + 32) },
             _ => continue,
         }
     }
+}
+
+
+fn enter_mode() -> (i8, bool) {
+    let mut mode = String::new();
+    io::stdin()
+        .read_line(&mut mode)
+        .expect("fail");
+
+    let mode: i8 = match mode.trim().parse() {
+        Ok(num) => num,
+        Err(_) => { return (0, false) },
+    };
+
+    match mode {
+        1..=2 => {},
+        _ => {
+            println!("Выбран некорректный режим");
+            return (0, false)
+        }
+    }
+    return (mode, true)
+}
+
+
+fn enter_temp() -> (i32, bool) {
+    println!("Введите температуру");
+    let mut temp = String::new();
+    io::stdin()
+        .read_line(&mut temp)
+        .expect("fail");
+
+    let temp: i32 = match temp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => { return (0, false) },
+    };
+
+    (temp, true)
 }
